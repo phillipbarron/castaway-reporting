cec263f3e485f4727e743cc76a6a207058faa521
Phill Barron <phillipbarron242@gmail.com>
2016-11-14 15:07:00 +0000

    added defined ranges

--- resources/views/publication/publication.php
+++ resources/views/publication/publication.php
@@ -51,6 +51,14 @@
                 locale: {
                     format: 'DD MMM YYYY'
                 },
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                },
                 "startDate": "11/06/2016",
                 "endDate": "11/12/2016"
                 }, function(start, end, label) {
